5e5e4c6a3fd974a310476c0752f19af1e3fd3e8a
feifan <lj19991213@gmail.com>
2021-08-13 23:11:50 +0800

    '初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'App',
+  data() {
+    return {
+      message:'666'
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
